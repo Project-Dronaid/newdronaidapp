54524391c02cbf1990d95e45fc1db47c13e8ff95
manaschanglani <manaschanglani10@gmail.com>
2024-07-08 09:54:35 +0530

    emergency screen created

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-const mobileBackgroundColor = Color.fromRGBO(84, 244, 255, 1);
-const dronaidblue = Color.fromARGB(255, 13, 57, 93);
-const dronaidblue2 = Color.fromARGB(255, 48, 82, 109);
+const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1); 
 const blueColor = Color.fromRGBO(0, 149, 246, 1);
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;

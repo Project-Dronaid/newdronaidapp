3d2b7d8f6a1d2a8db63ecc6dd937a5f0823af62d
Lucifer <gautam.gupta@tekditechnologies.com>
2024-07-26 17:07:46 +0530

    Updated Gautam branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,82 @@
+import 'package:dronaid_app/screens/OrderDelivered.dart';
+import 'package:dronaid_app/screens/emergency_page.dart';
+import 'package:dronaid_app/screens/home_page2.dart';
+import 'package:dronaid_app/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'screens/signUp/signUp.dart';
+import 'screens/login/login.dart';
+import 'utils/colors.dart';
+import 'screens/request_page.dart';
+import 'screens/ProfilePage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
 
-  final String title;
+  final List<Widget> _widgetOptions = <Widget>[
+    EmergencyPage(),
+    RequestPage(),
+    HomePage2(),
+    ProfilePage(),
+  ];
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _onItemTapped(int index) {
     setState(() {
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DronAid App',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        body: RequestConfirmedPage(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.request_page_outlined),
+              label: 'Requests',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline),
+              label: 'Info',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: 'Profile',
             ),
           ],
+          unselectedItemColor: Colors.black.withOpacity(0.6),
+          showUnselectedLabels: true,
+          currentIndex: _selectedIndex,
+          selectedItemColor: kPrimaryColor,
+          onTap: _onItemTapped,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      getPages: [
+        GetPage(name: '/login', page: () => LoginScreen()),
+        GetPage(name: '/signup', page: () => SignUpScreen()),
+      ],
     );
   }
 }
---
+++ lib/screens/signUp/signUp.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../login/login.dart';
+import 'signup_controller.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../utils/colors.dart';
+
+class SignUpScreen extends StatelessWidget {
+  final SignupController signupController = Get.put(SignupController());
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, 
+          children: [
+            SizedBox(height: size.height * 0.05), 
+            Center(
+              child: SvgPicture.asset(
+                'assets/signup.svg',
+                height: size.height * 0.5, 
+              ),
+            ),
+            SizedBox(height: size.height * 0.02), 
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: size.width * 0.1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start, 
+                children: [
+                  Text(
+                    "SIGN UP",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.03),
+                  Form(
+                    child: Column(
+                      children: [
+                        _buildTextField(signupController.hospitalNameController, "Hospital Name", Icons.local_hospital),
+                        _buildTextField(signupController.emailController, "Email", Icons.email),
+                        Obx(() => _buildTextField(
+                            signupController.passwordController,
+                            "Password",
+                            Icons.lock,
+                            isPassword: true,
+                            obscureText: signupController.obscureText.value,
+                            toggleObscureText: () {
+                              signupController.obscureText.value = !signupController.obscureText.value;
+                            }
+                        )),
+                        _buildTextField(signupController.addressController, "Address", Icons.location_on),
+                        _buildTextField(signupController.phoneController, "Phone Number", Icons.phone),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: size.width * 0.1, vertical: size.height * 0.02),
+              child: Obx(() => ElevatedButton(
+                onPressed: signupController.isLoading.value ? null : () {
+                  signupController.signup();
+                },
+                child: signupController.isLoading.value
+                    ? CircularProgressIndicator(color: primaryColor)
+                    : Text("SIGN UP", style: TextStyle(fontSize: 18, color: primaryColor)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kPrimaryColor,
+                  minimumSize: Size(size.width * 0.8, 50),
+                ),
+              )),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: size.width * 0.1, vertical: size.height * 0.02),
+              child: GestureDetector(
+                onTap: () {
+                  Get.to(() => LoginScreen()); 
+                },
+                child: Center(
+                  child: Text(
+                    "Already has an account? LOGIN",
+                    style: TextStyle(color: kPrimaryColor, fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String hintText, IconData icon, {bool isPassword = false, bool obscureText = false, Function? toggleObscureText}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon, color: kPrimaryColor),
+          hintText: hintText,
+          hintStyle: TextStyle(color: secondaryColor),
+          suffixIcon: isPassword ? IconButton(
+            icon: Icon(obscureText ? Icons.visibility : Icons.visibility_off),
+            onPressed: () {
+              if (toggleObscureText != null) toggleObscureText();
+            },
+          ) : null,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+    );
+  }
+}

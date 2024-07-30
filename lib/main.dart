81d5c15f32cc1f072c5a643b643aa1107f56f0c2
Lucifer <gautam.gupta@tekditechnologies.com>
2024-07-30 23:13:00 +0530

    merged with main and added screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:dronaid_app/screens/OrderDetails.dart';
 import 'package:dronaid_app/screens/emergency_page.dart';
 import 'package:dronaid_app/screens/fetched_emergency.dart';
 import 'package:dronaid_app/screens/home.dart';
@@ -47,8 +46,33 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: OrderTrackingPage(),
-     
+        body: SignUpScreen(),
+        // body:  _widgetOptions.elementAt(_selectedIndex),
+        // bottomNavigationBar: BottomNavigationBar(
+        //   items: const <BottomNavigationBarItem>[
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.home,),
+        //       label: 'Home',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.request_page_outlined,),
+        //       label: 'Requests',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.info_outline),
+        //       label: 'Info',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.person_outline),
+        //       label: 'Profile',
+        //     ),
+        //   ],
+        //   unselectedItemColor: Colors.black.withOpacity(0.6),
+        //   showUnselectedLabels: true,
+        //   currentIndex: _selectedIndex,
+        //   selectedItemColor: kPrimaryColor,
+        //   onTap: _onItemTapped,
+        // ),
       ),
     );
   }

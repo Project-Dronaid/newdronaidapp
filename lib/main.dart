5d37f3d24505b43e1216498c4189b4d03e69ae54
manaschanglani <manaschanglani10@gmail.com>
2024-08-24 19:40:30 +0530

    request route updated

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'DronAid',
       debugShowCheckedModeBanner: false,
       // home: OrderTrackingPage(),
       home: StreamBuilder(
